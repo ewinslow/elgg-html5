f9bebb9bf5580d922cca3715ef852cd28c12ff5c
Evan Winslow <evan.b.winslow@gmail.com>
2010-06-24 21:08:55 +0000

    Added IE support to html5 plugin

--- start.php
+++ start.php
@@ -1,1 +1,12 @@
 <?php
+
+function html5_init() {
+	
+	//This script must appear before any styles
+	elgg_extend_view('head/metas', 'scripts/html5', 1);
+	
+	elgg_extend_view('css', 'html5/css');
+	
+	elgg_view_register_simplecache('scripts/html5');
+	elgg_view_register_simplecache('html5/css');
+}
