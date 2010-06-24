1f8af59dc552d1a825d3575bff8e7ed403d125ba
Evan Winslow <evan.b.winslow@gmail.com>
2010-06-24 19:44:49 +0000

    Adding html5 plugin -- all it does now is provide the appropriate header

---
+++ views/default/page_elements/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * HTML5 header
+ * The standard HTML5 header that displays across the site
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ *
+ */
+
+// we won't trust server configuration but specify utf-8
+header('Content-type: text/html; charset=utf-8');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<?php echo elgg_view('page_elements/head', $vars); ?>
+<body>
