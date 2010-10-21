421822b135d524043b5ebfd07b1c62f6184e9891
Evan Winslow <evan.b.winslow@gmail.com>
2010-10-21 21:54:30 +0000

    Removed default placeholder from text inputs

--- views/default/input/text.php
+++ views/default/input/text.php
@@ -1,10 +1,1 @@
-<?php
-
-$defaults = array(
-	'placeholder' => elgg_echo('placeholder:text'),
-);
-
-$vars = array_merge($defaults, $vars);
-?>
-
 <input type="text" <?php echo html5_get_html_attributes($vars); ?> />
