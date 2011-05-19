888751ed186a8312fa482ae41a91eed0f8786d2b
Evan Winslow <evan@elgg.org>
2011-05-19 16:04:19 -0700

    Moved 3rd party javascripts to a vendors folder

--- start.php
+++ start.php
@@ -1,10 +1,10 @@
 <?php
 
 function html5_init() {
-	elgg_extend_view('css/screen', 'html5/css');
+	elgg_extend_view('css/elgg', 'html5/css', 1);
 
-	elgg_register_js('Modernizr', 'mod/html5/js/Modernizr-1.5.js', 'head', 1);
-	elgg_register_js('jquery.placeholder', 'mod/html5/js/jquery.placeholder-1.0.1.js', 'footer');
+	elgg_register_js('Modernizr', '/mod/html5/vendors/Modernizr-1.5.js', 'head', 1);
+	elgg_register_js('jquery.placeholder', '/mod/html5/vendors/jquery/jquery.placeholder-1.0.1.js', 'footer');
 }
 
 elgg_register_event_handler('init', 'system', 'html5_init');
